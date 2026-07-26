5da2ab62502a6e02fbf5913ca7915c380d98df7d
Bruno Van der Straten <bruno.vds@me.com>
2026-07-26 22:06:14 +0200

    Initial Commit

---
+++ Sub4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sub4
+//
+//  Created by Van der Straten Bruno on 26/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sub4/Sub4App.swift
@@ -0,0 +1,17 @@
+//
+//  Sub4App.swift
+//  Sub4
+//
+//  Created by Van der Straten Bruno on 26/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Sub4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
